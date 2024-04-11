9cd8352d810fd64230bdbc31a5e1d019d72d3f15
Kilo Loco <kyle@kiloloco.com>
2024-04-11 13:18:23 -0700

    Initial Commit

---
+++ TryStuff/TryStuffApp.swift
@@ -0,0 +1,17 @@
+//
+//  TryStuffApp.swift
+//  TryStuff
+//
+//  Created by Kilo Loco on 4/11/24.
+//
+
+import SwiftUI
+
+@main
+struct TryStuffApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
